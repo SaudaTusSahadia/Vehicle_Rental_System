13b88520804287a3d2190be2f0892a9a1f59f8a7
Sauda001 <sauda.cse8.bu@gmail.com>
2026-09-24 00:30:33 +0600

    Refactor createUser function to accept a payload object for improved parameter handling

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,7 +1,9 @@
 import { pool } from "../../config/db";
 
 
-const createUser = async(name: string, email: string, password: string, phone: string, role: string) =>{
+const createUser = async(payload: Record<string, unknown>) =>{
+    const { name, email, password, phone, role } = payload;
+    
     try {
     const result = await pool.query(
       'INSERT INTO users (name, email, password, phone, role) VALUES ($1, $2, $3, $4, $5) RETURNING *',
